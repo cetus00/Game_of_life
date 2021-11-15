5ee0deb506e042517456f9d08f66ca24744374c1
Ksenia Kuzina <6837643@gmail.com>
2021-11-15 19:09:12 +0100

    game of life template

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from game_of_life import GameOfLife
+
+
+
+app = Flask(__name__)
+# app.run(debug=True) # or in Terminal: export FLASK_DEBUG=1
+
+@app.route('/')
+def index():
+    GameOfLife(25,25)
+    return render_template("index.html")
+
+@app.route('/live')
+def live():
+    game = GameOfLife(25,25)
+    game.form_new_generation()
+    return render_template("live.html", game=game)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
